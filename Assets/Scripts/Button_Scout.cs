9bcb52a02efece40273bf3ec8fbba409f138f3ff
kameringring1029 <kameringring1029@gmail.com>
2017-08-03 21:47:15 +0900

    action part reinit

--- Assets/Scripts/Button_Scout.cs
+++ Assets/Scripts/Button_Scout.cs
@@ -5,20 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Button_Scout : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-
-    }
-
-    /* アイコンを選択時 */
-    public void OnMouseDown()
-    {
+    public void ButtonPush() { 
         Debug.Log("Scout pushed");
 
         SceneManager.LoadScene("Scout");
